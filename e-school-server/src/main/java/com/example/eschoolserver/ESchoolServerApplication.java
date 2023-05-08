2a39425f81e1a242b435e8c63a4ddbf5844cff8a
Temirlan Zhumagulov <91124777+TemirlanZhumagulov@users.noreply.github.com>
2023-05-08 16:53:11 +0600

    Update ESchoolServerApplication.java

--- e-school-server/src/main/java/com/example/eschoolserver/ESchoolServerApplication.java
+++ e-school-server/src/main/java/com/example/eschoolserver/ESchoolServerApplication.java
@@ -3,19 +3,13 @@ package com.example.eschoolserver;
 import com.example.eschoolserver.model.Student;
 import com.example.eschoolserver.model.User;
 import com.example.eschoolserver.service.StudentService;
-import org.apache.catalina.filters.CorsFilter;
 import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 
 
 @MappedTypes({Student.class, User.class})
